ae4519fd3e373e84456026f846111d8b935096d1
lodmev <lodmev@gmail.com>
2024-07-25 22:55:41 +0300

    feat: add possibility to write data in google sheets

---
+++ src/copy_leads.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php" ;
+
+$spreadsheetId = '1YhWBaejCte28BeVoTbwB4b5bomKphrtrks9TWtlpwOs';
+$client = new \Google_Client();
+$client->setApplicationName('Grab the leads');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('offline');
+$client->setAuthConfig(__DIR__ . '\..\credentials.json');
+$service = new Google_Service_Sheets($client);
+
+$range = 'one';
+$values = [["ONe", "two", "three", "four"]];
+$body = new Google_Service_Sheets_ValueRange(['values' => $values ]);
+$params = [
+  'valueInputOption' => 'RAW'
+];
+$insert = [
+  'insertDataOption' => 'INSERT_ROWS'
+];
+$result = $service->spreadsheets_values->append(
+  $spreadsheetId,
+  $range,
+  $body,
+  $params,
+  $insert
+);
